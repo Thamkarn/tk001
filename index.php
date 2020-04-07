31028640a1bd49b6df53be03a242781f064cb944
Thamkarn <63250463+Thamkarn@users.noreply.github.com>
2020-04-07 09:56:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,6 @@ function send_reply_message($url, $post_header, $post_body)
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     $result = curl_exec($ch);
     curl_close($ch);
-
     return $result;
 }
 
