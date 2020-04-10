797da79718a63d219872fa186e717b700de3ab9e
Thamkarn <63250463+Thamkarn@users.noreply.github.com>
2020-04-10 10:25:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
 echo "OK";
 
-$ch = curl_init("http://nextdevelop.ddns.net:81//testline/index.php?log=".$request."");
+$ch = curl_init("http://nextdevelop.ddns.net:81//testline/setdata.php?log=".$request."");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $responett = curl_exec($ch);
 curl_close($ch);
